c9f9f828478372979fcf1a39fb06b518771109af
Chris Winkler <procy.dev@gmail.com>
2021-09-01 16:35:11 -0700

    Maps setup, fetching resturants and displaying details of each

---
+++ src/api/index.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
+
+export const getPlacesData = async (sw, ne) => {
+    try {
+        const { data: { data } } = await axios.get(URL, {
+            params: {
+                bl_latitude: sw.lat,
+                tr_latitude: ne.lat,
+                bl_longitude: sw.lng,
+                tr_longitude: ne.lng,
+            },
+            headers: {
+                'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+                'x-rapidapi-key': '9719d938e3msh7d97adc2a13d575p1795f0jsn715d3503d950'
+            }
+        });
+
+        return data;
+    } catch (e) {
+        console.log(e);
+    }
+}
